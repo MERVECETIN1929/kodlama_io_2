f2c233267d87867a44971562e505d80436af1aaa
Merve Cetin <mervecetinnitec@gmail.com>
2022-10-01 12:09:18 +0300

    overriding

---
+++ overriding/src/overriding/Overriding.java
@@ -0,0 +1,12 @@
+package overriding;
+
+public class Overriding {
+
+    public static void main(String[] args) {
+        BaseKrediManager[] krediManagers = new BaseKrediManager[]{new OgretmenKrediManager(),new TarimKrediManager(),new OgrenciKrediManager()};
+        for (BaseKrediManager baseKrediManager : krediManagers) {
+            System.out.println(baseKrediManager.hesapla(1000));
+        }
+    }
+
+}
